c46228427cd56f2bf2c17172e95c21332f9259c4
babacar <nguiranebabacar305@gmail.com>
2024-05-15 23:25:55 +0000

    version 3

--- function/function.js
+++
@@ -1,8 +0,0 @@
-import {readFile} from 'node:fs/promises';
-
-const path='storage/storage.json'
-
-export async function findProduct(){
-    const chemin = await readFile(path,'utf8')
-    return JSON.parse(chemin)
-}
